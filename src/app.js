ea3db3268075a14c0f3512d33942f634197d1b4e
hartik teetwal <162800633+hartikteetwal@users.noreply.github.com>
2026-06-24 18:58:54 +0530

    chnages

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require("express")
 const authRouter = require("./routes/auth.route")
 const cookieParser = require("cookie-parser")
 const cors = require("cors")
-const interviewRouter = require("./routes/interview.Routes")
+const interviewRouter = require("./routes/interview.routes")
 
 const app = express()
 
